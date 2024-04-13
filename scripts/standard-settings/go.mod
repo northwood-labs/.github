8c155fec4c7909bd603c914ff313d7ee71d73914
Ryan Parman <ryan@ryanparman.com>
2024-04-13 12:17:29 -0600

    Standardized repo settings.

---
+++ scripts/standard-settings/go.mod
@@ -0,0 +1,20 @@
+module standard-settings
+
+go 1.22.2
+
+require (
+	github.com/google/go-github/v61 v61.0.0
+	github.com/northwood-labs/golang-utils/exiterrorf v0.0.0-20240405162342-5cc2ac09b2e9
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/northwood-labs/golang-utils/debug v0.0.0-20240405162342-5cc2ac09b2e9 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
---
+++ scripts/standard-settings/standard-settings.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/v61/github"
+	"github.com/northwood-labs/golang-utils/exiterrorf"
+	"github.com/pkg/errors"
+	flag "github.com/spf13/pflag"
+)
+
+var (
+	ctx = context.Background()
+
+	fRepo string
+)
+
+func main() {
+	ghToken := os.Getenv("GITHUB_TOKEN")
+	if ghToken == "" {
+		exiterrorf.ExitErrorf(errors.New("GITHUB_TOKEN is required"))
+	}
+
+	flag.StringVarP(&fRepo, "repo", "r", "", "Repository to update inside NWL organization")
+	flag.Parse()
+
+	client := github.NewClient(nil).WithAuthToken(ghToken)
+
+	_, _, err := client.Repositories.Edit(ctx, "northwood-labs", fRepo, &github.Repository{
+		AllowAutoMerge:      github.Bool(true),
+		AllowMergeCommit:    github.Bool(false),
+		AllowRebaseMerge:    github.Bool(false),
+		AllowSquashMerge:    github.Bool(true),
+		AllowUpdateBranch:   github.Bool(true),
+		Archived:            github.Bool(false),
+		DefaultBranch:       github.String("main"),
+		DeleteBranchOnMerge: github.Bool(true),
+		HasIssues:           github.Bool(true),
+		HasProjects:         github.Bool(false),
+		HasWiki:             github.Bool(false),
+		IsTemplate:          github.Bool(false),
+		SecurityAndAnalysis: &github.SecurityAndAnalysis{
+			SecretScanning: &github.SecretScanning{
+				Status: github.String("enabled"),
+			},
+			SecretScanningPushProtection: &github.SecretScanningPushProtection{
+				Status: github.String("enabled"),
+			},
+			SecretScanningValidityChecks: &github.SecretScanningValidityChecks{
+				Status: github.String("enabled"),
+			},
+		},
+	})
+	if err != nil {
+		exiterrorf.ExitErrorf(errors.Wrap(err, "failed to edit repository"))
+	}
+
+	fmt.Println("OK")
+}
